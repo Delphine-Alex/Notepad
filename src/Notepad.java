c1138fc83e9ed04bc317b209b6fa41edcbefb036
Delphine <delphine.pva@gmail.com>
2023-08-02 18:41:33 +0200

    menu bar and menu items

--- src/Notepad.java
+++ src/Notepad.java
@@ -1,5 +1,8 @@
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -8,6 +11,10 @@ public class Notepad {
 	JFrame window;
 	JTextArea textArea;
 	JScrollPane scrollPane;
+	JMenuBar menuBar;
+	
+	JMenu menuFile, menuEdit, menuFormat, menuColor;
+	JMenuItem menuItemNew, menuItemOpen, menuItemSave, menuItemSaveAs, menuItemExit;
 	
 	public static void main(String[] args) {
 		
@@ -19,6 +26,8 @@ public class Notepad {
 		
 		createWindow();
 		createTextArea();
+		createMenuBar();
+		createFileMenu();
 		
 		window.setVisible(true);
 		
@@ -42,6 +51,42 @@ public class Notepad {
 		window.add(scrollPane);
 		// window.add(textArea);
 		
+	}
+	
+	public void createMenuBar() {
+		
+		menuBar = new JMenuBar();
+		window.setJMenuBar(menuBar);
+		
+		menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		
+		menuEdit = new JMenu("Edit");
+		menuBar.add(menuEdit);
+		
+		menuFormat = new JMenu("Format");
+		menuBar.add(menuFormat);
+		
+		menuColor = new JMenu("Color");
+		menuBar.add(menuColor);
+	}
+	
+	public void createFileMenu() {
+		
+		menuItemNew = new JMenuItem("New");
+		menuFile.add(menuItemNew);
+		
+		menuItemOpen = new JMenuItem("Open");
+		menuFile.add(menuItemOpen);
+		
+		menuItemSave = new JMenuItem("Save");
+		menuFile.add(menuItemSave);
+		
+		menuItemSaveAs = new JMenuItem("Save as");
+		menuFile.add(menuItemSaveAs);
+		
+		menuItemExit = new JMenuItem("Exit");
+		menuFile.add(menuItemExit);
 	}
 	
 	
